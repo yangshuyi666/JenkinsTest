820b61e4a58e1d1bdbe7407d3dd9cf8bc59fa98d
yangshuyi666 <3424304208@qq.com>
2025-05-27 14:40:19 +0800

    Update FileUtilsTest.java

--- src/test/java/org/apache/commons/io/FileUtilsTest.java
+++ src/test/java/org/apache/commons/io/FileUtilsTest.java
@@ -34,41 +34,41 @@ public class FileUtilsTest {
     }
 
     @Test(timeout = 1000)
-    public void deleteQuietly() throws InterruptedException {
-        // 假设文件存在，调用 deleteQuietly 方法删除文件
-        // 然后断言文件仍然存在（故意让测试失败）
-//        FileUtils.deleteQuietly(testFile);
-//        assertTrue("文件应该被删除，但实际上仍然存在", testFile.exists());
-        //停止2秒
-        Thread.sleep(2000);
-        assertTrue("error",false);
+    public void deleteQuietlyTest() throws InterruptedException {
+         //假设文件存在，调用 deleteQuietly 方法删除文件
+         //然后断言文件仍然存在
+        FileUtils.deleteQuietly(testFile);
+        assertTrue("文件应该被删除，但实际上仍然存在", testFile.exists());
     }
 
     @Test
-    public void readFileToString() {
+    public void readFileToStringTest() {
         try {
             // 向文件中写入一些内容
-            FileUtils.writeStringToFile(testFile, "Hello, World!", "UTF-8");
+            FileUtils.writeStringToFile(testFile, "Hello, World!", "UTF-16");
             // 读取文件内容
-            String content = FileUtils.readFileToString(testFile, "UTF-8");
-            // 断言读取的内容与预期不符（故意让测试失败）
-            assertEquals("故意设置错误的预期内容", content, "Goodbye, World!");
+            String content = FileUtils.readFileToString(testFile, "UTF-16");
+            // 断言读取的内容与预期不符
+            assertEquals("", content, "Hello, World!");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void writeStringToFile() {
+    public void writeStringToFileTest() {
         try {
             // 向文件中写入内容
-            FileUtils.writeStringToFile(testFile, "Hello, World!", "UTF-8");
+            FileUtils.writeStringToFile(testFile, "Hello, World!\n", "UTF-8");
+            FileUtils.writeStringToFile(testFile, "Hello, World!\n", "UTF-8");
             // 再次读取文件内容
             String content = FileUtils.readFileToString(testFile, "UTF-8");
-            // 断言读取的内容与写入的内容不符（故意让测试失败）
-            assertNotEquals("内容应该相同，但实际上不同", content, "Hello, World!");
+            
+            assertNotEquals("内容应该相同，但实际上不同", content, "Hello, World!\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
 }
